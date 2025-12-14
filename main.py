9cf5e8769406b7c631ba8fdd06436b973e1173f0
herol3oy <hamedsedighi@gmail.com>
2025-12-14 16:51:31 +0100

    update take screenshot

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pytz
 from datetime import datetime
 import os
+import subprocess
 
 URLS_FILE = "urls.txt"
 OUTPUT_DIR = "screenshots"
@@ -21,3 +22,27 @@ def take_screenshots():
     print(f"\nScreenshot cycle {timestamp}")
     
     os.makedirs(OUTPUT_DIR, exist_ok=True)
+    
+    for url in urls:
+        temp_png = f"{OUTPUT_DIR}/{url}_{timestamp}.png"
+        final_webp = f"{OUTPUT_DIR}/{url}_{timestamp}.webp"
+        
+    print(f" > Capturing {url}")
+
+    command = [
+            "shot-scraper", url,
+            "-o", temp_png,
+            "--wait", "2000",
+            "--width", "1440",
+            "--height", "1080"
+        ]
+    
+    command.extend(["--javascript", "()=>{}"])
+    
+    try:
+        subprocess.run(command, check=True)
+        
+    except Exception as e:
+        print(f"Error capturing {url}: {e}")
+
+take_screenshots()
