dfe954d0a5622b49e6275743bcc00da67ac7de70
herol3oy <hamedsedighi@gmail.com>
2025-12-14 16:44:48 +0100

    add take screenshots

--- main.py
+++ main.py
@@ -1,8 +1,23 @@
+import pytz
+from datetime import datetime
+import os
+
 URLS_FILE = "urls.txt"
+OUTPUT_DIR = "screenshots"
 
 def load_urls():
-    with open(URLS_FILE, "r") as file:
-        for line in file:
-            print(line.strip())
-            
-load_urls()
+    if not os.path.exists(URLS_FILE):
+        raise FileNotFoundError(f"{URLS_FILE} not found")
+
+    with open(URLS_FILE) as file:
+        return [line.strip() for line in file if line.strip()]
+
+def take_screenshots():
+    urls = load_urls()
+    
+    cet = pytz.timezone("Europe/Warsaw")
+    timestamp = datetime.now(cet).strftime("%Y-%m-%d_%H-%M")
+    
+    print(f"\nScreenshot cycle {timestamp}")
+    
+    os.makedirs(OUTPUT_DIR, exist_ok=True)
